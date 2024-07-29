0284b1047fc6fb4773c94391682b36580a9edf2b
kailash <adm_az_ksahu@eaholding.onmicrosoft.com>
2024-07-29 12:37:31 +0530

    Check if the input starts with a custom delimiter definition

--- StringCalculator.java
+++ StringCalculator.java
@@ -7,11 +7,21 @@ public class StringCalculator {
         if (numbers == null || numbers.isEmpty()) {
             return 0;
         }
+        String[] delimiters = {",", "\n"};
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("\n");
+
+        }
+
         return 0;
     }
 
     public static void main(String[] args) {
         StringCalculator calculator = new StringCalculator();
-
+        System.out.println(calculator.add(""));         // Output: 0
+        System.out.println(calculator.add("1"));        // Output: 1
+        System.out.println(calculator.add("1,5"));      // Output: 6
+        System.out.println(calculator.add("1\n2,3"));   // Output: 6
+        System.out.println(calculator.add("//;\n1;2")); // Output: 3
     }
 }
