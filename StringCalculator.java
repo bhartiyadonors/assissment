636303708b32f13a48242e6468ecf08a0b38d228
kailash <adm_az_ksahu@eaholding.onmicrosoft.com>
2024-07-29 12:35:01 +0530

    added add string method this will definitly return something

---
+++ StringCalculator.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringCalculator {
+
+    public int add(String numbers) {
+        if (numbers == null || numbers.isEmpty()) {
+            return 0;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        StringCalculator calculator = new StringCalculator();
+
+    }
+}
